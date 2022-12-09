8341ea89d196e196c05adf8d10df14bf004b99c6
johnnion1 <jonathan.burak1@gmail.com>
2022-12-09 16:00:06 +0100

    Change divs to put 'paper-style' outline around text, refactor text

--- src/lander.js
+++ src/lander.js
@@ -5,19 +5,22 @@ const element = document.createElement('div');
 const landerImage = new Image();
 landerImage.className = 'landerImage'
 landerImage.src = Landingimage;
+const landerContent = document.createElement('div');
+landerContent.id = 'landerContent';
 
 const heading = document.createElement('h1');
 heading.className = 'landerHeading'
-heading.textContent = 'La Trattoia D`Alfredoe doe greeting!';
+heading.textContent = "Sie sind stets herzlich willkommen, bei Trattoria D`Alfredoe!"
 
 const mainText = document.createElement('p');
 mainText.className = 'landerText'
-mainText.textContent = "This traditional family restaurant is especially famous as numero uno calidad el mundía italiano tempranillo vino carta multiple winner! The Family Angeleios' ancestors sold Jamónes picantes y azucáres dulces and since then these are especialidades alfredoedos. The numerous cuisines are extraordinanti famosas All of the extranjeros se huelvan el agua en la boca. The story goes:     'Hace calientes noches si chef Alfredoede pone su bigote. Da èl el cuchillo, sweetie, andále porque estoy tenengo hambréro!' \n\nTienes hambre? Are you hungry? Come to Alfredoe's!"
+mainText.textContent = "Dieser Familienbetrieb, der 2014 unter Alberto Alfola Alfredoe die ersten Teller zur Speisung der Speckminenarbeiter im Sankt-Petrus Gau aufdeckte, lud schon damals sowohl reich und arm herzlich ein. In gemütlicher Amtosphäre servieren wir  Ihnen  Wein, Desserts und spitalienische Speisen, welche Chef Alfredoe stets nach altem Familienrezept zubereitet. Haben Sie Hunger? Tienes hambre? Are you hungry? Come to Alfredoe's! Kommen Sie uns alleine, oder auch als Gruppe besuchen. Sie sind stets Willkommen, bei Trattoria D`Alfredoe's!"
 
 element.id = 'landerPage'
-element.appendChild(heading);
-element.appendChild(mainText);
-element.appendChild(landerImage);
+landerContent.appendChild(heading);
+landerContent.appendChild(mainText);
+landerContent.appendChild(landerImage);
+element.appendChild(landerContent);
 
 return element;
 }
